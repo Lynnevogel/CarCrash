8a137781621938b4412aa9911a5655e263aeabfd
Lynnevogel <125022094+Lynnevogel@users.noreply.github.com>
2023-06-17 10:24:59 +0200

    Start visualisatie

---
+++ code/visualization/color_blocks.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualize_board(board, cars):
+    dim = len(board)
+    car_list = []
+
+    for car in cars:
+        car_list.append(car)
+
+    # Create a grid
+    grid = np.zeros((dim, dim), dtype=int)
+
+    # Define color mappings for each car using hex values
+    car_colors = {'A': '#f8371c',
+                  'B': '#f7175d',
+                  'C': '#b314e2',
+                  'D': '#6a2af2',
+                  'E': '#324def',
+                  'F': '#24cdf5',
+                  'G': '#16aa93',
+                  'H': '#43c92d',
+                  'I': '#fb8504',
+                  'J': '#d5e70f',
+                  'K': '#fcec61',
+                  'L': '#ffb800',
+                  'M': '#f09c39',
+                  'N': '#eb6437',
+                  'O': '#ff6561',
+                  'P': '#ff4f93',
+                  'Q': '#cb53f2',
+                  'R': '#9a69f9',
+                  'S': '#7181f6',
+                  'T': '#52bcff',
+                  'U': '#42dffe',
+                  'V': '#38cbb9',
+                  'W': '#6ae05e',
+                  'Y': '#a9e654',
+                  'AA': '#e3f23f',
+                  'AB': '#ff8e8e',
+                  'AC': '#ffb2d0',
+                  'AD': '#eab3f9',
+                  'AE': '#d1befc',
+                  'AF': '#c1c9f9',
+                  'AG': '#aee3ff',
+                  'AH': '#a2f4ff',
+                  'AI': '#98ecea',
+                  'AJ': '#bbf1bb',
+                  'AK': '#d8f7af',
+                  'AL': '#f4fa9c',
+                  'AM': '#fdf287',
+                  'AN': '#f8d669',
+                  'AO': '#f5b961',
+                  'AP': '#e2150a',
+                  'AQ': '#ae0456',
+                  'AR': '#2c0fd3',
+                  'AS': '#0f6972',
+                  'X': '#1f9dfa'}
+
+    # Loop through the board and assign values to the grid based on the car positions
+    for row in range(dim):
+        for col in range(dim):
+            if board[row][col] in car_list:
+                car = board[row][col]
+                grid[row][col] = car_list.index(car) + 1
+
+    figure_size = dim * (2/3)
+    # Display the plot in the terminal
+    fig, ax = plt.subplots(figsize=(figure_size, figure_size))
+
+    # Set the background color of the figure
+    fig.set_facecolor('#000010')  # Light gray
+
+    # Loop through the grid and plot each car as a block of color with the letter on top
+    for row in range(dim):
+        for col in range(dim):
+            car = board[row][col]
+            if car != '-':
+                color = car_colors[car]
+                ax.add_patch(plt.Rectangle((col, row), 1, 1, facecolor=color, edgecolor='#000010'))
+                ax.text(col + 0.5, row + 0.5, car, ha='center', va='center', fontsize=28, color='#000010', fontname='Monospace')
+
+    for row in range(dim):
+        if 'X' in board[row]:
+            exit_row = row
+            break
+
+    exit_col = dim - 1
+    exit_color = '#000010'
+    ax.add_patch(plt.Rectangle((exit_col, exit_row), 1, 1, facecolor=exit_color, edgecolor='#000010'))
+    ax.text(exit_col + 0.5, exit_row + 0.5, 'EXIT', ha='center', va='center', fontsize=13, color='#00ff7b', fontname='Monospace', fontweight='bold')
+
+    ax.set_xlim(0, dim + 0.5)
+    ax.set_ylim(0, dim + 0.5)
+    ax.grid(color='#fff', linewidth=0.3)
+    ax.axis('off')
+    ax.invert_yaxis()
+    # timer = fig.canvas.new_timer(interval = 1000)
+    # timer.add_callback(plt.close)
+    # timer.start()
+    plt.show()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from code.classes.board import Board
+from code.algorithms.random import random_move
+from code.visualization.color_blocks import visualize_board
 from sys import argv
 import time
 from code.algorithms.random import random_move
@@ -8,8 +10,10 @@ from code.algorithms.breadth import breadth_first
 if __name__ == "__main__":
     if len(argv) < 2:
         # Raise error if game name is not given
-        print("Usage: python main.py [game]")
+        print("Usage: python main.py [game] [algorithm]")
         exit(1)
+    elif len(argv) == 2:
+        algorithm = 'random'
     elif len(argv) == 3:
         algorithm = argv[2]
 
@@ -24,8 +28,8 @@ if __name__ == "__main__":
 
     # Run game until it is won
     while not board.is_won():
-        if algorithm == 'random' or len(argv) == 1:
-            # Random algorithm
+        # Random algorithm
+        if algorithm == 'random':
             # Pick a random car
             random_car = board.random_car()
             print(random_car)
@@ -47,13 +51,11 @@ if __name__ == "__main__":
             start_state = board
             breadth_first(start_state)
 
-        
-
         # Check if game is won
         if board.is_won():
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"Puzzle was solved in {elapsed_time}s.")
             print(f"Number of moves: {num_moves}")
-            # board.visualize_board()
+            visualize_board(board.board, board.cars)
             break
