7763d37ab5420a01fd170848e4db64755939bf66
Lynnevogel <125022094+Lynnevogel@users.noreply.github.com>
2023-06-24 13:49:07 +0200

    Main geupdatet

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from code.algorithms.breadth_first import BreadthFirst
 from code.algorithms.depth_first import DepthFirst
 from code.algorithms.astar import AStar
 from code.algorithms.hillclimber import HillClimber
-from experiment import start_time, end_time
+from experiment import start_time, end_time, output_experiment
 
 from sys import argv
 import time
@@ -26,47 +26,118 @@ if __name__ == "__main__":
     # Extract command line argument
     game_name = argv[1]
     # Initialize game
-    board = Board(game_name)
-    # Start timer
-    
+    board = Board(game_name)   
     
     for i in range(amount_of_times):
 # -------------------------------------- Random search ------------------------------------------------------------------------------
         if algorithm == 'random':
-            start_time()
+            start = start_time()
             random = Random(board)
             random.go()
+            elapsed_time = end_time(start)
+
+            # output
+            n = i + 1
+            game = game_name
+            algorithm = algorithm
+            dimension = board.dim
+            time = elapsed_time
+            won = 1
+
+            number_of_moves, number_of_states, solution = random.generate_output()
+            print(f"number of moves: {number_of_moves}")
+            print(f"number of states: {number_of_states}")
+            print(f"solution: {solution}")
+            output_experiment(n, game, algorithm, dimension, time, number_of_moves, number_of_states, won, solution)
+
             i += 1
-            end_time(start_time)
+
 # ------------------------------------- Breadth-first search ------------------------------------------------------------------------
-        elif algorithm == 'bf':
-            start_time()
+        elif algorithm == 'breadthfirst':
+            start = start_time()
             breadth_first = BreadthFirst(board)
             breadth_first.go()
+            elapsed_time = end_time(start)
+
+            # output
+            n = i + 1
+            game = game_name
+            algorithm = algorithm
+            dimension = board.dim
+            time = elapsed_time
+            won = 1
+
+            number_of_moves, number_of_states, solution = breadth_first.generate_output()
+            print(f"number of moves: {number_of_moves}")
+            print(f"number of states: {number_of_states}")
+            print(f"solution: {solution}")
+            output_experiment(n, game, algorithm, dimension, time, number_of_moves, number_of_states, won, solution)
+
             i += 1
-            end_time(start_time)
 # -------------------------------------- Depth-first search ------------------------------------------------------------------------
-        elif algorithm == 'df':
-            start_time()
+        elif algorithm == 'depthfirst':
+            start = start_time()
             depth_first = DepthFirst(board)
             depth_first.go()
+            elapsed_time = end_time(start)
+
+            # output
+            n = i + 1
+            game = game_name
+            algorithm = algorithm
+            dimension = board.dim
+            time = elapsed_time
+            won = 1
+
+            number_of_moves, number_of_states, solution = depth_first.generate_output()
+            print(f"number of moves: {number_of_moves}")
+            print(f"number of states: {number_of_states}")
+            print(f"solution: {solution}")
+            output_experiment(n, game, algorithm, dimension, time, number_of_moves, number_of_states, won, solution)
+
             i += 1
-            end_time(start_time)
 # ------------------------------------------ A* search ------------------------------------------------------------------------
         elif algorithm == 'astar':
-            start_time()
+            start = start_time()
             astar = AStar(board) 
             astar.go()
+            elapsed_time = end_time(start)
+
+            # output
+            n = i + 1
+            game = game_name
+            algorithm = algorithm
+            dimension = board.dim
+            time = elapsed_time
+            won = 1
+
+            number_of_moves, number_of_states, solution = astar.generate_output()
+            print(f"number of moves: {number_of_moves}")
+            print(f"number of states: {number_of_states}")
+            print(f"solution: {solution}")
+            output_experiment(n, game, algorithm, dimension, time, number_of_moves, number_of_states, won, solution)
+
             i += 1
-            end_time(start_time)
 # -------------------------------------- Hill Climber search ------------------------------------------------------------------------
         elif algorithm == 'hillclimber':
-            start_time()
-            random = Random(board)
-            random_solution = random.go()
-            solution = random_solution.directions
+            start = start_time()
+            hill_climber = HillClimber(board)
+            hill_climber.run_iterations(2)
+            elapsed_time = end_time(start)
 
-            hill_climber = HillClimber(random_solution, board, solution)
-            hill_climber.go(500)
+            # output
+            n = i + 1
+            game = game_name
+            algorithm = algorithm
+            dimension = board.dim
+            time = elapsed_time
+            won = 1
+
+            number_of_moves, number_of_states, solution, state_space = hill_climber.generate_output()
+            print(f"number of moves: {number_of_moves}")
+            print(f"number of states: {number_of_states}")
+            print(f"solution: {solution}")
+            print(f"state space: {state_space}")
+            output_experiment(n, game, algorithm, dimension, time, number_of_moves, number_of_states, won, solution, state_space)
+
             i += 1
-            end_time(start_time)
