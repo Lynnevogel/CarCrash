c8dad6e1985c6c5d6064854a5458ce731c837e1f
13143131 <isajk22@hotmail.com>
2023-06-25 09:35:48 +0200

    Experiment gerund + enkele docstrings
    6x6_1 random
    9x9_4 random
    6x6_2 hillclimber

--- code/heuristics/zero_heuristic.py
+++
@@ -1,9 +0,0 @@
-class ZeroHeuristic:
-    """
-    The zero heuristic return zero for every node.
-    """
-    def __init__(self):
-        pass
-    
-    def heuristic_value(self):
-        return 0
